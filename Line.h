53bd5e15fa3df0d7e185f1f19e0296ab22c49c39
eunjee97 <47707115+eunjee97@users.noreply.github.com>
2019-11-15 14:19:29 +0900

    Add files via upload

---
+++ Line.h
@@ -0,0 +1,23 @@
+#pragma once
+#pragma once
+#include "Light.h"
+#include "Vector3.h"
+#include <ctime>
+
+class Line
+{
+public:
+	Line();
+	void setPosition1(float x, float y, float z);
+	void setPosition2(float x, float y, float z);
+	Vector3 getPosition1()const;
+	Vector3 getPosition2()const;
+	void timeRun();
+	void setTime();
+
+private:
+	Vector3 position1;
+	Vector3 position2;
+	int itime;
+	int pass_time;
+};
